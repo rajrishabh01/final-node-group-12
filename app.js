135a09b98cf9b38e278e1f29804968988b6cde92
Rishabh-mac <raj.rishabh01@gmail.com>
2022-12-01 22:11:45 -0500

    Node js setup for group 12

---
+++ app.js
@@ -0,0 +1,6 @@
+import express from "express";
+
+const app = express();
+app.get('/hello', (req, res) => {res.send('Life is good!')})
+app.get('/', (req, res) => {res.send('Welcome to Full Stack Development!')})
+app.listen(4000);
