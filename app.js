388c80da56e1be4cf1b691d83c850b0cbfdfcf4d
Rishabh-mac <raj.rishabh01@gmail.com>
2022-12-01 23:52:22 -0500

    port fix

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 import express from "express";
+import cors from 'cors'
 
 const app = express();
 app.get('/hello', (req, res) => {res.send('Life is good!')})
 app.get('/', (req, res) => {res.send('Welcome to Full Stack Development!')})
-app.listen(4000);
+
+app.use(express.json());
+app.use(cors());
+
+app.listen(process.env.PORT || 4000)
