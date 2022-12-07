0c92e378774c1be0b7a19db3f2f75499d40e024f
Rishabh-mac <raj.rishabh01@gmail.com>
2022-12-07 00:37:31 -0500

    session-controller issue

--- app.js
+++ app.js
@@ -4,7 +4,9 @@ import session from 'express-session'
 import RecipesController from "./controllers/recipes/recipes-controller.js";
 import mongoose from "mongoose";
 import HelloController from "./controllers/hello-controller.js";
-import UsersController2 from "./controllers/users/user-controller-single.js";
+//import UsersController2 from "./controllers/users/user-controller-single.js";
+import SessionController from "./session-controller.js";
+import UsersController from "./controllers/users/users-controller.js";
 
 
 //const CONNECTION_STRING = process.env.DB_CONNECTION_STRING || 'mongodb://localhost:27017/whipup';
@@ -25,24 +27,23 @@ mongoose.connect(CONNECTION_STRING, options);
 console.log("Connected to MongoDB: " + CONNECTION_STRING);
 
 const app = express();
-// app.options('*', cors())
-// app.use(cors({
-//     credentials: true,
-//     origin: 'http://localhost:3000'
-// }))
-
-// app.use(session({
-//     secret: 'should be an environment variable',
-//     resave: false,
-//     saveUninitialized: true,
-//     cookie: { secure: false }
-// }))
+app.use(cors({
+    credentials: true,
+    origin: 'http://localhost:3000'
+}))
+app.use(session({
+    secret: 'should be an environment variable',
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: false }
+}))
 
 app.use(express.json());
-app.use(cors());
 HelloController(app);
-UsersController2(app);
+//UsersController2(app);
+UsersController(app);
 RecipesController(app);
+SessionController(app);
 
 app.listen(process.env.PORT || 4000);
 console.log("Server started, listening on port " + (process.env.PORT || 4000));
