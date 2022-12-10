16985cf5b8133d77a5c7a60f46c25e0bec542d6f
wantingzeng <zeng.wan@northeastern.edu>
2022-12-10 12:20:30 -0500

    Revert "useSelector for search result by ingredients"
    This reverts commit bbf38474109ccdfa0804670c4913671e97e82222.

--- controllers/reviews/reviews-dao.js
+++
@@ -1,7 +0,0 @@
-import reviewsModel from "./reviews-model.js";
-
-export const createReview = async (review) =>
-        await reviewsModel.create(review)
-
-export const deleteReview = async (rid) =>
-    await reviewsModel.deleteOne({_id:rid})
--- controllers/reviews/reviews-model.js
+++
@@ -1,6 +0,0 @@
-import mongoose from "mongoose"
-import reviewsSchema from "./reviews-schema.js";
-
-const reviewsModel = mongoose.model(
-    'ReviewsModel', reviewsSchema)
-export default reviewsModel
